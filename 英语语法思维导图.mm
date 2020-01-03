352f6f5528fec44972c10b80726ebfced2b3ef56
bilifo <2734174334@qq.com>
2020-01-03 19:12:43 +0800

    2020/1/3

---
+++ 英语语法思维导图.mm
@@ -0,0 +1,202 @@
+
+<map version="0.9.0">
+    <node CREATED="0" ID="52c44963aca74ffe8bf1b52721f5d5ff" MODIFIED="0" TEXT="语法">
+        <node CREATED="0" ID="b427ee41852c49c882c6f228f1c77c3e" MODIFIED="0" POSITION="right" TEXT="​词类">
+            <node CREATED="0" ID="246c9202fa4243e6834343314adb2dea" MODIFIED="0" POSITION="right" TEXT="​实义词">
+                <node CREATED="0" ID="fde6898e50094a828d4d5053e6595e96" MODIFIED="0" POSITION="right" TEXT="​副词.修饰或限制动词和形容词，表示范围、程度等.如:都,只,再三,屡次,很,也">
+                    <node CREATED="0" ID="8d521efb18684c08a6957c663fc07954" MODIFIED="0" POSITION="right" TEXT="​普通副词">
+                        <node CREATED="0" ID="4199f5ab144e478bb981147a6074c092" MODIFIED="0" POSITION="right" TEXT=""/>
+                    </node>
+                    <node CREATED="0" ID="9fc0baaf79c04ce2ad4a9f7f901416b6" MODIFIED="0" POSITION="right" TEXT="​疑问副词">
+                        <node CREATED="0" ID="03580406896145059038eaeb62cf8d95" MODIFIED="0" POSITION="right" TEXT="​when(何时)/where(何地)/how(如何)/why(为什么)等"/>
+                    </node>
+                    <node CREATED="0" ID="17f3b31a33cd4e1d93ab083f99c283a1" MODIFIED="0" POSITION="right" TEXT="​连接副词">
+                        <node CREATED="0" ID="4a9c91628564447fa033d293285ac0a7" MODIFIED="0" POSITION="right" TEXT="​therefor(因此)/then(然后)/however(然而)/otherwise(否则)等"/>
+                    </node>
+                    <node CREATED="0" ID="c439ab6354844df18257d854016c207d" MODIFIED="0" POSITION="right" TEXT="​关系副词">
+                        <node CREATED="0" ID="ae09423f5f304052a1bc3f9825891a18" MODIFIED="0" POSITION="right" TEXT="​where/when/why"/>
+                    </node>
+                </node>
+                <node CREATED="0" ID="759acbd5a55e43eb9a0df96a1f9821c3" MODIFIED="0" POSITION="right" TEXT="​动词,表示动作和状态的词,如签字sign,存在exist">
+                    <node CREATED="0" ID="3b779148e4394c7faa8cccbe4a4c5c71" MODIFIED="0" POSITION="right" TEXT="​实义动词(有完整的意义,可独立做谓语)">
+                        <node CREATED="0" ID="525e82d85b9f47f9ad4300ccd18c2fcb" MODIFIED="0" POSITION="right" TEXT="​解释explain,停留stay"/>
+                    </node>
+                    <node CREATED="0" ID="0648f60db3d643c093f91d4942939f2e" MODIFIED="0" POSITION="right" TEXT="​情态动词,表说话人的语气和情感">
+                        <node CREATED="0" ID="78b1273820364365b7d093e377f50018" MODIFIED="0" POSITION="right" TEXT="​can(能),may(可以),must(必须)等+动词"/>
+                    </node>
+                    <node CREATED="0" ID="3d6993eb34b840b1930c0c2e1c9547d0" MODIFIED="0" POSITION="right" TEXT="​助动词,和主要动词一起构成各种时态,语态,语气等动词形式或用于疑问,否定结构中">
+                        <node CREATED="0" ID="1572c3ed2a8c4b02bb2ea2e7dfec0df1" MODIFIED="0" POSITION="right" TEXT="​shall/will/have/be/should/sould/do"/>
+                    </node>
+                    <node CREATED="0" ID="86741361748348c48d11bfc831a4ca40" MODIFIED="0" POSITION="right" TEXT="​实义动词">
+                        <node CREATED="0" ID="e6c00c24fc214da9a04a412a7995cf67" MODIFIED="0" POSITION="right" TEXT="​及物动词,后面必须要加上动作对象"/>
+                        <node CREATED="0" ID="8aba62ecb42a467dbe08f0f869de4a05" MODIFIED="0" POSITION="right" TEXT="​不及物动词,后面没有动作对象"/>
+                    </node>
+                    <node CREATED="0" ID="432862010ceb4b96b33c3a4d259b5858" MODIFIED="0" POSITION="right" TEXT="​系动词">
+                        <node CREATED="0" ID="b8fee12e92fb4defb19511a0d7229a77" MODIFIED="0" POSITION="right" TEXT="​be(是)/seem(似乎)/look(看起来)/become(变成)/get(变得)/feel(感到)/turn(变成)等"/>
+                    </node>
+                    <node CREATED="0" ID="7df31207e4e049cdb4bcba6f3fce361f" MODIFIED="0" POSITION="right" TEXT="--​动词过去式">
+                        <node CREATED="0" ID="be5dae0a416146d6a2c55ec03d5c27c5" MODIFIED="0" POSITION="right" TEXT="​规则">
+                            <node CREATED="0" ID="3d6fbe716725470cbd1e96702663ec1d" MODIFIED="0" POSITION="right" TEXT="​在原来的动词后面+ed或+d"/>
+                        </node>
+                        <node CREATED="0" ID="4df54f4f160443f594be177904cedba2" MODIFIED="0" POSITION="right" TEXT="​不规则">
+                            <node CREATED="0" ID="37e7b904efcb45f98dae6cdf2f0cf947" MODIFIED="0" POSITION="right" TEXT="​不定,有表,可查,常用要记,不为什么,因为要考"/>
+                        </node>
+                    </node>
+                    <node CREATED="0" ID="de1802e438b74f4d93b539f6cb468ad6" MODIFIED="0" POSITION="right" TEXT="​--动词现在进行时">
+                        <node CREATED="0" ID="9af923da35f14de2b844cbdb8a70f782" MODIFIED="0" POSITION="right" TEXT="规则">
+                            <node CREATED="0" ID="b985fc125576434ba7b1d11cfc7b76f4" MODIFIED="0" POSITION="right" TEXT="​​原来动词+ing"/>
+                        </node>
+                        <node CREATED="0" ID="dba29dfde4c14108b191372cbfb316ef" MODIFIED="0" POSITION="right" TEXT="​不规则"/>
+                    </node>
+                </node>
+                <node CREATED="0" ID="cbc0cf066db34b57b49b45639f32b807" MODIFIED="0" POSITION="right" TEXT="​形容词,带'的',形容词一般放在所修饰的名词前">
+                    <node CREATED="0" ID="36e27418bc3645d9872d32a7cddda0bf" MODIFIED="0" POSITION="right" TEXT="​原级">
+                        <node CREATED="0" ID="6191c9a538204d2e809c4d4e1b1de4de" MODIFIED="0" POSITION="right" TEXT="​普通形容词,美丽的,健壮的"/>
+                    </node>
+                    <node CREATED="0" ID="584386911fa74854802c77bf660f7775" MODIFIED="0" POSITION="right" TEXT="​比较级">
+                        <node CREATED="0" ID="8424fc606aaa4aa8b39198195777cf17" MODIFIED="0" POSITION="right" TEXT="​形容词+后缀+er 或 more+形容词,更美丽的,健壮得多的"/>
+                    </node>
+                    <node CREATED="0" ID="0e80c5ef77ae4797a211350f87655e85" MODIFIED="0" POSITION="right" TEXT="​最高级">
+                        <node CREATED="0" ID="ad9a058044e0423a861c80f6ed033462" MODIFIED="0" POSITION="right" TEXT="​形容词+后缀+est 或most+形容词,最美丽的,最强壮的"/>
+                    </node>
+                </node>
+                <node CREATED="0" ID="cac816e3634e4aa7a0ac2b6e9840577d" MODIFIED="0" POSITION="right" TEXT="​数词">
+                    <node CREATED="0" ID="78e2b16cbc484497ab2e334c69ef7870" MODIFIED="0" POSITION="right" TEXT="​基数词,表数量(one/twe等)"/>
+                    <node CREATED="0" ID="b7b160a6c248409fbceea38a05a9118b" MODIFIED="0" POSITION="right" TEXT="​序数词,表顺序(first 等)"/>
+                </node>
+                <node CREATED="0" ID="de4c5a08aa9647cbb6aecf8b2a40ae22" MODIFIED="0" POSITION="right" TEXT="​代词">
+                    <node CREATED="0" ID="6507f8322d664c8190fb98d71300cc21" MODIFIED="0" POSITION="right" TEXT="​人称代词">
+                        <node CREATED="0" ID="5a18b9c14b0143f0984418f0c4bc9fb1" MODIFIED="0" POSITION="right" TEXT="​I/you/they/it 等"/>
+                    </node>
+                    <node CREATED="0" ID="910b6e518a1843c7b57e64e82e4984bb" MODIFIED="0" POSITION="right" TEXT="​物主代词">
+                        <node CREATED="0" ID="bca805104a1a4fa39fb7eb8feb32283e" MODIFIED="0" POSITION="right" TEXT="​my/his/their/our/mine/hers 等"/>
+                    </node>
+                    <node CREATED="0" ID="bc4f98d1c768422cab5314026b491a19" MODIFIED="0" POSITION="right" TEXT="​反身代词">
+                        <node CREATED="0" ID="e2098d143a904f409d07a606a6274046" MODIFIED="0" POSITION="right" TEXT="​myself/youself/itself/ourselves/oneself 等"/>
+                    </node>
+                    <node CREATED="0" ID="aaaf59264c9e402fb0034c7aa6617951" MODIFIED="0" POSITION="right" TEXT="​相互代词">
+                        <node CREATED="0" ID="ff3b72a2b44a45b2a4453d7f0ee7f4e2" MODIFIED="0" POSITION="right" TEXT="each other,one another等"/>
+                    </node>
+                    <node CREATED="0" ID="68a3f5ee80fb422591e8658e2006b09c" MODIFIED="0" POSITION="right" TEXT="​指示代词">
+                        <node CREATED="0" ID="0d540cd54edf43cd8f7cb74730aba3a8" MODIFIED="0" POSITION="right" TEXT="​this/that/these/those/such/same 等"/>
+                    </node>
+                    <node CREATED="0" ID="0e0ace13c60d45a8b6b1d0c13149804e" MODIFIED="0" POSITION="right" TEXT="​疑问代词">
+                        <node CREATED="0" ID="6e57622013e94ee1af6a8b3907279f01" MODIFIED="0" POSITION="right" TEXT="​who/whom/whose/which/what 等"/>
+                    </node>
+                    <node CREATED="0" ID="8a8177008fec439cb50d53d5512ffddc" MODIFIED="0" POSITION="right" TEXT="​关系代词">
+                        <node CREATED="0" ID="e857caa797e64c2f8ee5a0bf7d2b0abc" MODIFIED="0" POSITION="right" TEXT="​who/whom/whose/which/that 等"/>
+                    </node>
+                    <node CREATED="0" ID="beffc5f04de04015ac290bdaaa0327b3" MODIFIED="0" POSITION="right" TEXT="​不定代词">
+                        <node CREATED="0" ID="c3b3bf8c87c7482899f68c49e45a90f6" MODIFIED="0" POSITION="right" TEXT="​some/any/no/all/one/every/many/a little/someone/anything 等"/>
+                    </node>
+                </node>
+                <node CREATED="0" ID="23671c43afcb4a7ca076a653458bd8d6" MODIFIED="0" POSITION="right" TEXT="​名词">
+                    <node CREATED="0" ID="41edf55bfe244d87aa72e12b2f89639c" MODIFIED="0" POSITION="right" TEXT="​普通名词(表某一类的统称)"/>
+                    <node CREATED="0" ID="7bcae8714323416ca80ff6a3592b62f0" MODIFIED="0" POSITION="right" TEXT="​专有名词(特定人或物,首字母大写)"/>
+                </node>
+            </node>
+            <node CREATED="0" ID="2087d5ce54f54354b9814077fa2df193" MODIFIED="0" POSITION="right" TEXT="虚词,无实义">
+                <node CREATED="0" ID="2e4485e48a924b0da2e0871350c1e6ef" MODIFIED="0" POSITION="right" TEXT="​介词,又叫前置词,放名词/代词前,表示后面的词或句子,和格式化字符串的$s一样">
+                    <node CREATED="0" ID="35d1153a62144ab0ab6f1a3df128fd02" MODIFIED="0" POSITION="left" TEXT="​简单介词">
+                        <node CREATED="0" ID="82c1c26f1530414dbfc1b6cdc87dc40c" MODIFIED="0" POSITION="left" TEXT="​in/at/for/since"/>
+                    </node>
+                    <node CREATED="0" ID="b78b79bf9259461f83383315851b1bbf" MODIFIED="0" POSITION="left" TEXT="​符合介词">
+                        <node CREATED="0" ID="e8457998d6f84c0ea324752375390395" MODIFIED="0" POSITION="left" TEXT="​into(进入)/as for(至于)/out of(出自)"/>
+                    </node>
+                    <node CREATED="0" ID="70bc322be5b64bd0b583fab41f3511ee" MODIFIED="0" POSITION="left" TEXT="​二重介词">
+                        <node CREATED="0" ID="e1a5b69e970a4848b58e6d9a9f8cbe67" MODIFIED="0" POSITION="left" TEXT="​until after(直至..之后)/from among(从..当中)"/>
+                    </node>
+                    <node CREATED="0" ID="3b5e701b01224b5aa8182308a8ec7bb3" MODIFIED="0" POSITION="left" TEXT="短语介词">
+                        <node CREATED="0" ID="49da323520f04072acde59ab3e927e79" MODIFIED="0" POSITION="left" TEXT="​according to(根据)/because of(因为)/in front of(在..之前)/in the event of(如果)"/>
+                    </node>
+                    <node CREATED="0" ID="6cff2a5359f44be0b343f6e4c179a1de" MODIFIED="0" POSITION="left" TEXT="​分词介词">
+                        <node CREATED="0" ID="56adbba9bf57497fa1289e24656ee16d" MODIFIED="0" POSITION="left" TEXT="​regarding(关于)/considering(考虑到)/including(包括)"/>
+                    </node>
+                </node>
+                <node CREATED="0" ID="5be396184b2a4bfd9a5d6dda215b1753" MODIFIED="0" POSITION="right" TEXT="​冠词(放在名词前,帮助说明名词所指对象)">
+                    <node CREATED="0" ID="bea6b8025bfb4b7eaa2030e07401661f" MODIFIED="0" POSITION="right" TEXT="​不定冠词">
+                        <node CREATED="0" ID="276b60e160c0460fb6067e7eeabcb7c6" MODIFIED="0" POSITION="right" TEXT="​a(以辅音开头的名词)/an(以元音开始的名词)"/>
+                    </node>
+                    <node CREATED="0" ID="d11b4f4326044495959bcc830530e3da" MODIFIED="0" POSITION="right" TEXT="​定冠词">
+                        <node CREATED="0" ID="ed18eedc02db4a3f8437b7084097350b" MODIFIED="0" POSITION="right" TEXT="​the(表特指的某类人或物)"/>
+                    </node>
+                </node>
+                <node CREATED="0" ID="0fb52436107c4a4d9035c16f11ae17fe" MODIFIED="0" POSITION="right" TEXT="​感叹词">
+                    <node CREATED="0" ID="a2b31f6a73ba487e9ea4b2f348f01db7" MODIFIED="0" POSITION="right" TEXT="​oh/well/why/hello等"/>
+                </node>
+                <node CREATED="0" ID="1c0df47267dd44c59556d62042321a15" MODIFIED="0" POSITION="right" TEXT="​连词,连接词">
+                    <node CREATED="0" ID="eb8981bc736d4f398f2c40d7a0881727" MODIFIED="0" POSITION="right" TEXT="​并列连词">
+                        <node CREATED="0" ID="5c08a6e251974519a2e25bae6d2b71ec" MODIFIED="0" POSITION="right" TEXT="​and/or/but/for(因为)/not only..but also(不仅..而且)/neither..not(即不..也不)"/>
+                    </node>
+                    <node CREATED="0" ID="2fee83a96a384170b3c1876a279258cd" MODIFIED="0" POSITION="right" TEXT="​从属连词">
+                        <node CREATED="0" ID="72d4970213bd4539b29f50394e62a38c" MODIFIED="0" POSITION="right" TEXT="​that/if/whether(是否)/when(当..时)/although(虽然)/because(因为)/so that(结果)"/>
+                    </node>
+                </node>
+            </node>
+        </node>
+        <node CREATED="0" ID="c306657b872e492f9b491a0070e97843" MODIFIED="0" POSITION="left" TEXT="​句子成分">
+            <node CREATED="0" ID="8baf51e26874408bac853d497c7f28a7" MODIFIED="0" POSITION="left" TEXT="​主语">
+                <node CREATED="0" ID="e90b919cdf2846ec81c18bda2edc2f06" MODIFIED="0" POSITION="left" TEXT="​句子所要说明的人或物,是句子的主体.一般在句首,不省略">
+                    <node CREATED="0" ID="94d6d1a9b6b24306aedd41c5524546ce" MODIFIED="0" POSITION="left" TEXT="​Walls have ears(墙有耳朵)墙是主体,表明它有耳朵,墙是主语"/>
+                </node>
+            </node>
+            <node CREATED="0" ID="a586174e819d4f7088e466a776bdfaad" MODIFIED="0" POSITION="left" TEXT="​谓语动词">
+                <node CREATED="0" ID="07e8b53ec6634e99a9332b3c58370337" MODIFIED="0" POSITION="left" TEXT="​是对主语加以判断,说明,描写，指出“做什么”do what,“是什么”what is this或是“怎么样”how,一般在主语之后">
+                    <node CREATED="0" ID="a00b35f9970d4b609521645806175245" MODIFIED="0" POSITION="left" TEXT="​Tom was very sick at heart(汤姆心里非常难受)sick是形容词作谓语,表示tom的状态"/>
+                </node>
+            </node>
+            <node CREATED="0" ID="5e31b8510a37403087026962c0f4a3cb" MODIFIED="0" POSITION="left" TEXT="​表语">
+                <node CREATED="0" ID="b8988049ebb04422883a623c9f2d8b5d" MODIFIED="0" POSITION="left" TEXT="​用来说明主语的身份,特征和状态,它的位置在系动词后面">
+                    <node CREATED="0" ID="ef3b6fb118644d4a9a438680bc9aa603" MODIFIED="0" POSITION="left" TEXT=""/>
+                </node>
+            </node>
+            <node CREATED="0" ID="66cfd2b1475a49acaa594a7bc039ca15" MODIFIED="0" POSITION="left" TEXT="​宾语">
+                <node CREATED="0" ID="f23058a6dc2f44d190068eb3878a6019" MODIFIED="0" POSITION="left" TEXT="​相对于主语的另外一位角色"/>
+            </node>
+            <node CREATED="0" ID="d00806a0bfe64395b5c8036fe9755ad8" MODIFIED="0" POSITION="left" TEXT="定语">
+                <node CREATED="0" ID="d86ec0875af34732b033beebe3f806fc" MODIFIED="0" POSITION="left" TEXT="​修饰限定主语或宾语"/>
+            </node>
+            <node CREATED="0" ID="62bcd1b0150a419abb8efb69a4661c6b" MODIFIED="0" POSITION="left" TEXT="​状语">
+                <node CREATED="0" ID="dfff4f4fd6574193a19b0baae30b3255" MODIFIED="0" POSITION="left" TEXT="​时间,地点,原因,条件,目的,结果,比较等"/>
+            </node>
+            <node CREATED="0" ID="584c44d4b2b5426a8d6b5b58d230d1e4" MODIFIED="0" POSITION="left" TEXT="​主语补足语"/>
+            <node CREATED="0" ID="7d1dc1d1b6004f9a814f0681464a8722" MODIFIED="0" POSITION="left" TEXT="​宾语补足语"/>
+        </node>
+        <node CREATED="0" ID="844888a71e5347209a65f89d27dcbfb7" MODIFIED="0" POSITION="left" TEXT="​基本句型">
+            <node CREATED="0" ID="f3cddf7463594a79b9cad304212f11ed" MODIFIED="0" POSITION="right" TEXT="​主---系---表"/>
+            <node CREATED="0" ID="7e5f0019e09342b88ef9780773905f00" MODIFIED="0" POSITION="right" TEXT="​主---动"/>
+            <node CREATED="0" ID="2b06b6f2e0eb4d509618b1335c56b3c3" MODIFIED="0" POSITION="right" TEXT="​主---动---宾"/>
+            <node CREATED="0" ID="f76a3ced2baf44a188a943c1f6b20757" MODIFIED="0" POSITION="right" TEXT="​主---动---宾---宾"/>
+            <node CREATED="0" ID="45d2356e55a54fc595c9b46680bcce8b" MODIFIED="0" POSITION="right" TEXT="​主---动---宾---补"/>
+            <node CREATED="0" ID="0eaa1b21565f4f299e9cfcefb74d36fb" MODIFIED="0" POSITION="right" TEXT="​主---谓"/>
+            <node CREATED="0" ID="7d6a8306fd3f44be893f126d114916ca" MODIFIED="0" POSITION="right" TEXT="​主---谓---宾"/>
+            <node CREATED="0" ID="46bfa35d650d419999b76380bb906d9f" MODIFIED="0" POSITION="right" TEXT="​​主---谓---宾---宾"/>
+            <node CREATED="0" ID="f2a535762e714218b70930eddba8a979" MODIFIED="0" POSITION="right" TEXT="​​主---谓---宾---补"/>
+        </node>
+        <node CREATED="0" ID="dbab7e14fb574c54b9d1aa750089d4a5" MODIFIED="0" POSITION="left" TEXT="​构词法,词根">
+            <node CREATED="0" ID="406115113a004a19bce66dd8dba46b1f" MODIFIED="0" POSITION="right" TEXT="​派生词">
+                <node CREATED="0" ID="ae5050c9706b46b4ab4a2210ad3f1587" MODIFIED="0" POSITION="right" TEXT="​构成名词的常用后缀">
+                    <node CREATED="0" ID="9bc6d599a0ae4f56aa6ee58c085f0d34" MODIFIED="0" POSITION="right" TEXT="​-er/-or/-ist/-ee/-ese/-ant   人或物的名词,如thinker(思想家)/translator(翻译者)"/>
+                    <node CREATED="0" ID="eff25a9bbae743448874ccac1dfed5c1" MODIFIED="0" POSITION="right" TEXT="​-ance/-ence/-(a)tion/-sion/-ics/-ing/-ity/-ment/-ness/-th/-ty/-ure/-ship  用于个欧城行为,性质,状态的名词 如:acceptance(接受)/repetition(重复)"/>
+                </node>
+                <node CREATED="0" ID="a12b17559ff14d2bb9790f2f732fea56" MODIFIED="0" POSITION="right" TEXT="​构成形容词的常用后缀">
+                    <node CREATED="0" ID="f10d29d77a1c436cb33b9634bacc754a" MODIFIED="0" POSITION="right" TEXT="​-able/-ible/-al/-ful/-less/-ish/-ive/-ous/-an/-ic/-ly/-ant/-ent/-ary/-en  如:failthful(忠诚的)/powerful(强大的)/available(可获得的)"/>
+                </node>
+                <node CREATED="0" ID="edf3e2abc36346aa80a70d2deead66ca" MODIFIED="0" POSITION="right" TEXT="​构成副词的常用后缀">
+                    <node CREATED="0" ID="13c9f58ab7dd48df91ed1a2affaa1567" MODIFIED="0" POSITION="right" TEXT="​-ly/-ward/-wise 如:terribly(可怕的)/otherwise(否则)"/>
+                </node>
+                <node CREATED="0" ID="44f17cef5c054ae4a722cc3a1613e442" MODIFIED="0" POSITION="right" TEXT="​构成动词的常用后缀">
+                    <node CREATED="0" ID="47d64992e0c040aeaa36f48e44c7d95f" MODIFIED="0" POSITION="right" TEXT="​-ise/-ize/-en/-ify 如:realize(实现)/verify(证实)/modify(修饰)"/>
+                </node>
+            </node>
+            <node CREATED="0" ID="6537843d37264b8d8dac0be9b16ea7c0" MODIFIED="0" POSITION="right" TEXT="​前缀">
+                <node CREATED="0" ID="fb1521cd7e504bbdb749cd9598149cd1" MODIFIED="0" POSITION="right" TEXT="​表相反的意思的前缀">
+                    <node CREATED="0" ID="3a0d7c9f822c4885a685f0733e7f69d6" MODIFIED="0" POSITION="right" TEXT="​un-/dis/in-/im-/ir-/il-/de- 如:unhappy(不高兴) unload(卸货) dislike(不喜欢)"/>
+                </node>
+                <node CREATED="0" ID="6fa2377022d545668af3ace8dc367796" MODIFIED="0" POSITION="right" TEXT="表其他意思的前缀">
+                    <node CREATED="0" ID="e88e53a7d6874ba2997051892f3fe6ae" MODIFIED="0" POSITION="right" TEXT="​re-(重新)/mis-(错误的)/anti-(反,防)/ex-(前)/pre-(预先)/post-(后的)/fore-(先得)/self-(自我的,自动的)/super-(超级,上层)/sub-(下面的)/inter-(之间的,相互)/trans-(跨越,移)/ultra-(外,极)/semi-(半)/vice-(副)/uni-(单)/bi-(双)/multi-(多)/auto-(自动)"/>
+                </node>
+            </node>
+        </node>
+    </node>
+</map>
